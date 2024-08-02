3d450833337129cdcdc7e6e17c5f066f5d4a3681
DocLivsey <niktoizvatnikak79@gmail.com>
2024-08-02 12:37:32 +0300

    deleted atol`s vars

--- testing/config.py
+++ testing/config.py
@@ -4,18 +4,12 @@ from request.parsing.delete_props import delete_properties
 
 
 properties_file = 'resources/props.properties'
-properties = read_properties(properties_file)
+properties_data = read_properties(properties_file)
 
 
-URL = 'url'
+ATOL_URL = 'atol.url'
 TOKEN = 'token'
 GROUP_CODE = 'group.code'
 ATOL_LOGIN = 'atol.login'
 ATOL_PASSWORD = 'atol.password'
 
-
-__url = properties[URL]
-__token = properties[TOKEN]
-__group_code = properties[GROUP_CODE]
-__atol_login = properties[ATOL_LOGIN]
-__atol_password = properties[ATOL_PASSWORD]
