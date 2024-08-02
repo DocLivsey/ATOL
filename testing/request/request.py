bf7f1429c27ae04b1182d78d42c583b2131daccf
DocLivsey <niktoizvatnikak79@gmail.com>
2024-08-02 09:11:15 +0300

    reformat output in take_response

--- testing/request/request.py
+++ testing/request/request.py
@@ -3,18 +3,18 @@ from parsing.from_props import read_properties
 
 
 class Request:
-    
+
     def __init__(
-        self, 
-        properties='../props.properties', 
-        url_props_name='url',
-        json='./make_check/register_test_sell_check.json', 
-        method:str=None
+            self,
+            properties='../props.properties',
+            url_props_name='url',
+            json='./make_check/register_test_sell_check.json',
+            method: str = None
     ):
         self.__method = method
         self.__response = None
         self.__load_props__(properties, url_props_name)
-        
+
     def __load_props__(self, properties, url_props_name):
         props = read_properties(properties)
         self.__url = props[url_props_name]
@@ -23,12 +23,12 @@ class Request:
         }
         self.__json = None
         self.__data = None
-        self.__params = None 
-        
+        self.__params = None
+
     def request_with_response(self):
         self.make_request()
         self.take_response()
-    
+
     def make_request(self):
         if self.__method.lower() == 'post':
             self.__response = requests.get(
@@ -46,14 +46,13 @@ class Request:
                 data=self.__data,
                 params=self.__params,
             )
-            
+
     def take_response(self):
         print(
-        f'''
-        Response: {self.__response}\n
-        As json: {self.__response.json}\n
-        Reason: {self.__response.reason}\n
-        Text: {self.__response.text}\n
-        '''
-    )
-    
+            f'''
+            Response: {self.__response}\n
+            As json: {self.__response.json}\n
+            Reason: {self.__response.reason}\n
+            Text: {self.__response.text}\n
+            '''
+        )
