e15ed82e9381e48b31ed9d4f618145958bb237e9
DocLivsey <niktoizvatnikak79@gmail.com>
2024-08-02 11:28:00 +0300

    make config for env variables

--- testing/config.py
+++ testing/config.py
@@ -1,6 +1,21 @@
-from dotenv import dotenv_values
+from request.parsing.from_props import read_properties
+from request.parsing.to_props import rewrite_properties, write_properties
+from request.parsing.delete_props import delete_properties
 
 
-config = dotenv_values()
+properties_file = 'resources/props.properties'
+properties = read_properties(properties_file)
 
 
+URL = 'url'
+TOKEN = 'token'
+GROUP_CODE = 'group.code'
+ATOL_LOGIN = 'atol.login'
+ATOL_PASSWORD = 'atol.password'
+
+
+__url = properties[URL]
+__token = properties[TOKEN]
+__group_code = properties[GROUP_CODE]
+__atol_login = properties[ATOL_LOGIN]
+__atol_password = properties[ATOL_PASSWORD]
