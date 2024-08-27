6b6a446e50c7bb1310d0e7d71d4ea21b47f26cc7
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-08-27 09:54:05 +0300

    make temporal GET token and POST receipt

--- testing/main.py
+++ testing/main.py
@@ -1,3 +1,7 @@
 from testing.request.make_cheque.post_new_cheque import post_new_cheque, default_path_to_json
+from testing.request.request_token.get_request import get_token
+
 
 post_new_cheque(default_path_to_json, 'sell', create_uuid_automaticly=True)
+
+#get_token(properties_file='props.properties')
